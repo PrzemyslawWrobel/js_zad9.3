69520006ecc5c8da9beffbdc2376ee37704c9b92
Przemysław Wróbel <przemek.wrobel@poczta.fm>
2018-02-09 20:35:49 +0100

    Poprawiny błąd zmiennej globalnej

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 	
 var dinosaur = 'triceratops';
-	dinosaurUpperCased = dinosaur.toUpperCase();
+var dinosaurUpperCased = dinosaur.toUpperCase();
 
 console.log(dinosaurUpperCased);
 
